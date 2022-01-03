9615572ba51f1890d29dda407184d0d287f89aaf
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-01-03 13:04:13 -0500

    saved solutions

--- 8kyu/Multiple of index.js
+++ 8kyu/Multiple of index.js
@@ -10,3 +10,22 @@
 // [-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]
 
 
+
+
+
+// Solution:
+function multipleOfIndex(array) {
+    // creating a result array where we'll save the needed elements
+    let res = [];
+  
+    // loop over the array
+    for (let i = 0; i < array.length; i++) {
+      // check if the current element (array[i]) is a multiple of it's index (i) and if it is...
+      if (array[i] % i === 0) {
+        // add it to the resulting array
+        res.push(array[i]);
+      }
+    }
+  
+    return res;
+  }
