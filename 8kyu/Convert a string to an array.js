6dfe8f0bdad614c667afad048e78665b2da3fc15
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-01-05 13:17:31 -0500

    added solution

--- 8kyu/Convert a string to an array.js
+++ 8kyu/Convert a string to an array.js
@@ -12,3 +12,20 @@
 
 // Solution:
 
+function stringToArray(string){
+    return string.trim().split(" ")
+    }
+    
+    
+    //Found this answer on the internet. It makes a bit of sense
+    
+    
+    function stringToArray(string){
+      return string.split(' ');
+    }
+    
+    //This is the best way to answer the question. It is saying to take in the string, to add the split() method with a space in between shown by the (' '). And it is a simple return case. Very elegant. Learn from this
+    
+    
+    
+    
