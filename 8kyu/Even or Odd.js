37912ca5271bf87f139aaf2ee6b816daa0a06926
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-01-07 13:24:05 -0500

    added solutions

--- 8kyu/Even or Odd.js
+++ 8kyu/Even or Odd.js
@@ -6,3 +6,18 @@
 
 // Solution:
 
+function even_or_odd(number) {
+    if (number%2 == 0) {
+      return "Even";
+    } else {
+      return "Odd";
+    }
+  }
+  
+  
+  
+  
+  function even_or_odd(number) {
+    return number % 2 ? "Odd" : "Even"
+  }
+  
