d7c19c5ff7b8b85d1069418676a7d959814ba9b4
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-01-23 13:54:47 -0500

    added solutions

--- 8kyu/Are You Playing Banjo.js
+++ 8kyu/Are You Playing Banjo.js
@@ -12,3 +12,20 @@
 // Solution:
 
 
+function areYouPlayingBanjo(name) {
+    return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo";
+  }
+  
+  
+  
+  function areYouPlayingBanjo(name) {
+    const char = 'R'  
+    const charLC = "r"
+    if(name.startsWith(char) || name.startsWith(charLC)){
+      return name + " plays banjo"
+    }else{
+      return name + " does not play banjo"
+    }
+  }
+
+  
