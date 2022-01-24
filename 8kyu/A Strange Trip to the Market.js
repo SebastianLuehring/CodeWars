4cd5b46b388478d17e4d1a6dbafed3d27598ffee
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-01-24 13:57:23 -0500

    added solution

--- 8kyu/A Strange Trip to the Market.js
+++ 8kyu/A Strange Trip to the Market.js
@@ -9,3 +9,12 @@
 // Solution:
 
 
+function isLockNessMonster(s) {
+    if(s.includes("tree fiddy") || s.includes("3.50") || s.includes("three fifty")){
+      return true
+    }else{
+      return false
+    }
+  }
+
+  
