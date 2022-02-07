025f85a56af8c6a4096fc5d84397375da98d791f
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-02-07 15:39:39 -0500

    added solutions

--- 8kyu/Square(n) Sum.js
+++ 8kyu/Square(n) Sum.js
@@ -8,3 +8,17 @@
 // Solution:
 
 
+function squareSum(numbers) {
+    let sum = 0
+    for( let i = 0; i < numbers.length; i++) {
+        sum += numbers[i] **2
+    }
+    return sum
+}
+
+
+function squareSum(numbers){
+  return numbers.reduce(function(sum, n){
+    return (n*n) + sum;
+  }, 0)
+}
