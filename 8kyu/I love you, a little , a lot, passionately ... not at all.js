e927c5e5ed823219213def90a25731c6b8aac66a
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-02-09 15:53:47 -0500

    question prompt

--- 8kyu/I love you, a little , a lot, passionately ... not at all.js
+++ 8kyu/I love you, a little , a lot, passionately ... not at all.js
@@ -15,25 +15,3 @@
 // Solution:
 
 
-function howMuchILoveYou(nbPetals) {
-    let arr = ['I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all'];
-    for (let i = 0; i <= nbPetals; i++){
-    arr.push(arr[i]);
-    }
-    return arr[nbPetals-1];
-    }
-    
-    
-    
-    const phrases = [
-        'I love you',
-        'a little',
-        'a lot',
-        'passionately',
-        'madly',
-        'not at all',
-    ]
-    
-    function howMuchILoveYou(n) {
-         return phrases[(n - 1) % phrases.length] 
-    }
