ce4fac3fe736a7a21d28d271f4ad4e102042102d
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-02-16 16:14:37 -0500

    added solution

--- 8kyu/Alan Partridge II - Apple Turnover.js
+++ 8kyu/Alan Partridge II - Apple Turnover.js
@@ -18,3 +18,12 @@
 // Solution:
 
 
+function apple(x){
+    if((x * x) > 1000){
+      return `It's hotter than the sun!!`
+    }else{
+      return 'Help yourself to a honeycomb Yorkie for the glovebox.'
+    }
+  }
+
+  
