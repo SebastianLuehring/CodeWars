8e245d7bdfa675b1bb94becf41419e799fe615d6
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-02-27 16:44:53 -0500

    added solution

--- 8kyu/Training JS #7 if..else and ternary operator.js
+++ 8kyu/Training JS #7 if..else and ternary operator.js
@@ -13,4 +13,18 @@
 
 // Solution:
 
+function saleHotdogs(n){
+    if(n < 5){
+      return n * 100
+    }else if (n >= 5 && n < 10){
+      return n * 95
+    }else if (n >= 10){
+      return n * 90
+    }
+  }
+  
+  function saleHotdogs(n){
+    return n*(n<5?100:n<10?95:90);
+  }
 
+  
