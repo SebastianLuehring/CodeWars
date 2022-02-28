2e0c9eff081ebb8996388012ed450420dfb10791
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-02-28 16:47:56 -0500

    question prompt

--- 8kyu/Training JS #8 Conditional statement--switch.js
+++ 8kyu/Training JS #8 Conditional statement--switch.js
@@ -19,36 +19,4 @@
 // Solution:
 
 
-function howManydays(month){
-    var days;
-    switch (month){
-    case 2:
-        return 28
-        break;
-    case 4:
-    case 6:
-    case 9:
-    case 11:
-        return 30
-        break;
-        default:
-        return 31
-    }
-    return days;
-  }
-  
-  
-  
-  
-  function howManydays(month){
-    switch (month){
-       case 2: return 28
-       case 4:
-       case 6:
-       case 9:
-       case 11: return 30
-    }
-    return 31
-  }
 
-  
