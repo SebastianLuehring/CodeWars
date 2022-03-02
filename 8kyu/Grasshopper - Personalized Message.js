f554fcbef9f88c7af6b36172b91fea5fcdb042b9
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-03-02 16:55:12 -0500

    added solutions

--- 8kyu/Grasshopper - Personalized Message.js
+++ 8kyu/Grasshopper - Personalized Message.js
@@ -11,3 +11,17 @@
 // Solution:
 
 
+function greet (name, owner) {
+    if(name === owner){
+      return `Hello boss`
+    }else{
+      return `Hello guest`
+    }
+  }
+  
+  function greet (name, owner) {
+    return name === owner ? 'Hello boss' :  'Hello guest';
+  }
+
+
+  
