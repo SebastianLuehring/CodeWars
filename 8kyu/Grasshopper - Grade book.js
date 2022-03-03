dbc0379ce02bc8415c0f4b3b362fc65b5b5f9864
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-03-03 16:58:14 -0500

    added solutions

--- 8kyu/Grasshopper - Grade book.js
+++ 8kyu/Grasshopper - Grade book.js
@@ -16,3 +16,30 @@
 // Solution:
 
 
+function getGrade (s1, s2, s3) {
+    let score = (s1 + s2 + s3) / 3
+    if( 90 <= score && score <= 100 ){
+      return 'A'
+    }else if( 80 <= score && score < 90 ){
+      return 'B'
+    }else if ( 70 <= score && score < 80 ){
+      return 'C'
+    }else if ( 60 <= score && score < 70 ){
+      return 'D'
+    }else if ( 0 <= score && score < 60 ){
+      return "F"
+    }
+  }
+  
+  
+  function getGrade (s1, s2, s3) {
+    avg = (s1+s2+s3)/3;
+    if (avg < 60)  return "F";
+      else if (avg < 70) return "D";
+      else if (avg < 80) return "C";
+      else if (avg < 90) return "B";
+      else return "A";
+  }
+
+
+  
