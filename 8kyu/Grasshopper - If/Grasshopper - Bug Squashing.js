f92b760c2344ca12c4e3edb37f97a436f5a60fcb
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-03-08 17:41:54 -0500

    added solution

--- 8kyu/Grasshopper - If/Grasshopper - Bug Squashing.js
+++ 8kyu/Grasshopper - If/Grasshopper - Bug Squashing.js
@@ -14,3 +14,16 @@
 
 // Solution:
 
+var health = 100
+var position = 0
+var coins = 0
+
+function main () {
+     rollDice()
+     move()
+     combat()
+     getCoins()
+     buyHealth()
+     printStatus()
+}
+
