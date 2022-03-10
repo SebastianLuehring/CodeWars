4fcb468e1e1b2dc340d21c92020cbf05dd07dae5
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-03-10 17:44:19 -0500

    question prompt

---
+++ 8kyu/Grasshopper - If/Grasshopper - Variable Assignment Debug.js
@@ -0,0 +1,11 @@
+// Grasshopper - Variable Assignment Debug
+
+
+// Variable assignment
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+
+
+// Solution:
+
+
