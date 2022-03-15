9bd93129730e54c0c25e7cf8fc6e49eb0571992e
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-03-15 18:54:57 -0400

    added solutions

--- 8kyu/Grasshopper - If/Grasshopper - Check for factor.js
+++ 8kyu/Grasshopper - If/Grasshopper - Check for factor.js
@@ -14,4 +14,17 @@
 
 // Solution:
 
+function checkForFactor (base, factor) {
+    if (base % factor === 0){
+      return true
+    }else {
+      return false
+    }
+  }
+  
+  
+  function checkForFactor (base, factor) {
+    return base % factor === 0;
+  }
 
+  
