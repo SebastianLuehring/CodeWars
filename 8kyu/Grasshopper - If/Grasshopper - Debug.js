ca337c00f88c3e0860b1246c7ba8ecdff809afec
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-03-16 18:56:37 -0400

    added solution

--- 8kyu/Grasshopper - If/Grasshopper - Debug.js
+++ 8kyu/Grasshopper - If/Grasshopper - Debug.js
@@ -12,3 +12,14 @@
 // Solution:
 
 
+function weatherInfo (temp) {
+    let c = (temp - 32) * (5/9)
+    if (c > 0){
+          return (c + " is above freezing temperature")
+    }
+    else{
+          return (c + " is freezing temperature")
+    }
+  }
+
+  
