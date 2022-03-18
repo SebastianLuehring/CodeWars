caff7060968c9d82832b794f38cd424baadd62ae
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-03-18 18:58:26 -0400

    added solutions

--- 8kyu/Grasshopper - If/Thinkful - Logic Drills Traffic light.js
+++ 8kyu/Grasshopper - If/Thinkful - Logic Drills Traffic light.js
@@ -11,3 +11,24 @@
 // Solution:
 
 
+function updateLight(current) {
+  
+    if(current === "green"){
+      return "yellow"
+    }else if(current === "yellow"){
+      return "red"
+    }else if(current === "red"){
+      return "green"
+    }
+  
+  }
+  
+  
+  
+  function updateLight(current) {
+    
+    return current === 'yellow' ? 'red' : current === 'green' ? 'yellow' : 'green';
+  
+  }
+
+  
