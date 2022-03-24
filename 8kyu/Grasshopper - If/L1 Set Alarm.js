bed2f72744387ccb498e220ea87eefd8d39e9e42
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-03-23 23:50:29 -0400

    added solution

--- 8kyu/Grasshopper - If/L1 Set Alarm.js
+++ 8kyu/Grasshopper - If/L1 Set Alarm.js
@@ -11,3 +11,16 @@
 
 
 // Solution:
+
+
+function setAlarm(employed, vacation){
+    if(employed === true && vacation === false){
+      return true
+    }else{
+      return false
+    }
+  }
+  
+  
+  const setAlarm = (employed, vacation) => employed && !vacation;
+  
