bccfd9ef71c4aeca2d35862af76cdeb14e6edd8a
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-03-29 00:06:50 -0400

    added solutions

--- 8kyu/Grasshopper - If/Is n divisible by x and y.js
+++ 8kyu/Grasshopper - If/Is n divisible by x and y.js
@@ -12,4 +12,17 @@
 
 // Solution:
 
+function isDivisible(n, x, y) {
+    if( n % x === 0 && n % y === 0){
+      return true
+    }else {
+      return false
+    }
+  }
+  
+  
+  function isDivisible(n, x, y) {
+    return n % x === 0 && n % y === 0
+  }
 
+  
