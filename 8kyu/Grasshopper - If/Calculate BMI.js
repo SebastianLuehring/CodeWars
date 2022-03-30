46e718e8137b38d4038e1c9d9b5c1621968ea31f
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-03-30 00:08:29 -0400

    added solution

--- 8kyu/Grasshopper - If/Calculate BMI.js
+++ 8kyu/Grasshopper - If/Calculate BMI.js
@@ -14,3 +14,17 @@
 // Solution:
 
 
+function bmi(weight, height) {
+    let bodyMassIndex = (weight / (height * height))
+    if(bodyMassIndex <= 18.5){
+      return "Underweight"
+    }else if (bodyMassIndex <= 25.0){
+      return "Normal"
+    }else if(bodyMassIndex <= 30.0){
+      return "Overweight"
+    }else if(bodyMassIndex > 30){
+      return "Obese"
+    }
+  }
+
+  
