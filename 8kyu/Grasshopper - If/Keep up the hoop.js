8899c7734198d0d766ea6847e51e4af43f97701c
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-04-02 00:19:39 -0400

    added solutions

--- 8kyu/Grasshopper - If/Keep up the hoop.js
+++ 8kyu/Grasshopper - If/Keep up the hoop.js
@@ -11,3 +11,15 @@
 // Solution:
 
 
+function hoopCount (n) {
+    if(n >= 10){
+      return "Great, now move on to tricks"
+    }  else{
+      return "Keep at it until you get it"
+    }
+ }
+ 
+ 
+ function hoopCount (n) {
+   return (n < 10) ? 'Keep at it until you get it' : 'Great, now move on to tricks';
+ }
