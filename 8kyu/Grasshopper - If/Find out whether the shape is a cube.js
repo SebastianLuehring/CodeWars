eb4a012f94f11d0f9ac7ffae9b2b320d63bf6a46
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-04-05 00:54:46 -0400

    added solution

--- 8kyu/Grasshopper - If/Find out whether the shape is a cube.js
+++ 8kyu/Grasshopper - If/Find out whether the shape is a cube.js
@@ -13,3 +13,14 @@
 // Solution:
 
 
+var cubeChecker = function(volume, side){
+    if(volume <= 0 || side <= 0){
+      return false
+    }else if(volume === (side * side * side)){
+      return true
+    }else{
+      return false
+    }
+  };
+
+  
