a9099c6da9622bfc0f4dbc8c48dc4ecb3a259283
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-04-07 00:58:31 -0400

    added solution

--- 8kyu/Grasshopper - If/Determine offspring sex based on genes XX and XY chromosomes.js
+++ 8kyu/Grasshopper - If/Determine offspring sex based on genes XX and XY chromosomes.js
@@ -12,3 +12,10 @@
 // Solution:
 
 
+function chromosomeCheck(sperm) {
+    if(sperm === "XY"){
+      return "Congratulations! You're going to have a son."
+    }else if (sperm === "XX"){
+      return "Congratulations! You're going to have a daughter."
+    }
+  }
