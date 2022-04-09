5e5b1fced01b2d0b758672ca8169cf70c77e8c5b
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-04-09 01:03:20 -0400

    added solutions

--- 8kyu/Leonardo Dicaprio and Oscars.js
+++ 8kyu/Leonardo Dicaprio and Oscars.js
@@ -15,3 +15,40 @@
 // Solution:
 
 
+function leo(oscar){
+    if(oscar === 88){
+      return "Leo finally won the oscar! Leo is happy"
+    }else if( oscar === 86){
+      return "Not even for Wolf of wallstreet?!"
+    }else if(oscar < 88 && oscar !== 86){
+      return "When will you give Leo an Oscar?"
+    }else if(oscar > 88){
+      return "Leo got one already!"
+    }
+  }
+  
+  
+  const leo = (oscar) => {
+    return oscar === 88 ? 'Leo finally won the oscar! Leo is happy' :
+           oscar === 86 ? 'Not even for Wolf of wallstreet?!'       :
+           oscar  <  88 ? 'When will you give Leo an Oscar?'        :
+           'Leo got one already!';
+  };
+  
+  
+  
+  function leo(oscar){
+    switch (oscar){
+      case 88:
+        return "Leo finally won the oscar! Leo is happy";
+        break;
+      case 86:
+        return  "Not even for Wolf of wallstreet?!";
+        break;
+      default:
+        if(oscar<88) return "When will you give Leo an Oscar?";
+        else return  "Leo got one already!";
+    }
+  }
+  
+  
