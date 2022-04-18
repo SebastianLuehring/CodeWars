b6d5776844859bc1795c00484b4e4634114dfce2
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-04-18 13:51:59 -0400

    question prompt

--- 8kyu/How many lightsabers do you own.js
+++ 8kyu/How many lightsabers do you own.js
@@ -15,3 +15,16 @@
 // Solution:
 
 
+function howManyLightsabersDoYouOwn(name) {
+    if(name === "Zach"){
+      return 18
+    }else {
+      return 0
+    }
+  }
+  
+  function howManyLightsabersDoYouOwn(name) {
+    return name === 'Zach' ? 18 : 0;
+  }
+
+  
