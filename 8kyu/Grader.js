155eaf8e585597cb53cd37ef2cfbec4c48a22fe1
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-04-19 13:53:40 -0400

    added solution

--- 8kyu/Grader.js
+++ 8kyu/Grader.js
@@ -21,3 +21,27 @@
 // Solution:
 
 
+function grader(score) {
+    if(score > 1 || score < .6){
+      return "F"
+    }else if(score >= .9){
+      return "A"
+    }else if(score >= .8){
+      return "B"
+    }else if(score >= .7){
+      return "C"
+    }else if(score >= .6){
+      return "D"
+    }
+  }
+  
+  
+  function grader(score) {
+    if (score>1||score<0.6) return 'F'
+    if (score<0.7) return 'D'
+    if (score<0.8) return 'C'
+    if (score<0.9) return 'B'
+    return 'A'
+  }
+
+  
