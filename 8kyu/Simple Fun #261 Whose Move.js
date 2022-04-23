3bad7737c99d176bcb57ec1258c3e6de5f13de58
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-04-23 14:00:45 -0400

    added solutions

--- 8kyu/Simple Fun #261 Whose Move.js
+++ 8kyu/Simple Fun #261 Whose Move.js
@@ -16,3 +16,21 @@
 
 // Solution:
 
+
+function whoseMove(lastPlayer, win) {
+    if( lastPlayer === "black" && win === false){
+      return "white"
+    }else if( lastPlayer === "black" && win === true){
+      return "black"
+    }else if( lastPlayer === "white" && win === false){
+      return "black"
+    }else{
+      return "white"
+    }
+  }
+  
+  function whoseMove(lastPlayer, win) {
+    return win?lastPlayer:lastPlayer=="white"?"black":"white"
+  }
+
+  
