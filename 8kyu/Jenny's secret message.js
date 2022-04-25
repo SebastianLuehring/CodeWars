044a497a3ee310aef65bf588b610642edb6dfe3d
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-04-25 14:03:43 -0400

    added solutions

--- 8kyu/Jenny's secret message.js
+++ 8kyu/Jenny's secret message.js
@@ -9,3 +9,18 @@
 // Solution:
 
 
+function greet(name){
+    if(name === "Johnny"){
+          return "Hello, my love!";
+    }else{
+        return `Hello, ${name}!`;
+    }
+  }
+  
+  
+  
+  function greet(name){
+    return "Hello, " + (name == "Johnny" ? "my love" : name) + "!";
+  }
+
+  
