aa4006592bec635c8a5b225e7b86ed1d971db2c8
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-04-26 14:05:38 -0400

    added solutions

--- 8kyu/Total amount of points.js
+++ 8kyu/Total amount of points.js
@@ -16,3 +16,25 @@
 // Solution:
 
 
+function points(games) {
+    let result = [];
+    let score = 0;
+    games.forEach(x => {
+      result = x.split(":");
+      if(parseInt(result[0])>parseInt(result[1]))
+        score += 3;
+      else if(result[0]==result[1])
+        score += 1;
+      else
+        score += 0;
+    })
+    return score;
+  }
+  
+  
+  const points=games=>games.reduce((output,current)=>{
+      return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
+    },0)
+
+    
+    
