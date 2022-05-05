1c50498b978164f70af7a098ede8fce76c401d41
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-05-05 14:21:57 -0400

    added solutions

--- 8kyu/Counting sheep....js
+++ 8kyu/Counting sheep....js
@@ -16,3 +16,29 @@
 
 // Solution:
 
+
+function countSheeps(arrayOfSheep) {
+    const arr1 = arrayOfSheep
+    const count1 = arr1.filter(value => value === true).length;
+    return count1;
+  }
+  
+  
+  function countSheeps(arrayOfSheeps) {
+    return arrayOfSheeps.filter(Boolean).length;
+  }
+  
+  
+  
+  function countSheeps(arrayOfSheep) {
+    // TODO May the force be with you
+    var num = 0;
+    
+    for(var i = 0; i < arrayOfSheep.length; i++)
+      if(arrayOfSheep[i] == true)
+        num++;
+        
+    return num;
+  }
+
+  
