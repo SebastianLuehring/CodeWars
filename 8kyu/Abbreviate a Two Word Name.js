2e00ad85bfabbe7a2d74822be145175090c30399
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-05-08 14:30:17 -0400

    added solutions

--- 8kyu/Abbreviate a Two Word Name.js
+++ 8kyu/Abbreviate a Two Word Name.js
@@ -13,3 +13,19 @@
 // Solution:
 
 
+function abbrevName(name){
+
+    const x = name.split(' ').map(x => x[0]).join('.').toUpperCase();
+    return x
+}
+
+
+
+
+function abbrevName(name){
+
+  var nameArray = name.split(" ");
+  return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
+}
+
+
