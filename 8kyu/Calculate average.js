083171cbab0022bf6430de1e24b57e9d83f55941
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-05-12 14:36:38 -0400

    added solutions

--- 8kyu/Calculate average.js
+++ 8kyu/Calculate average.js
@@ -11,3 +11,17 @@
 // Solution:
 
 
+function find_average(array) {
+    if(array.length === 0){
+       return 0
+    }else{
+       return array.reduce((a,b) => a + b, 0) / array.length
+    }
+    }
+    
+    
+    function find_average(arr) {
+        return arr.length > 0? arr.reduce((a, b) => a + b) / arr.length : 0;
+    }
+
+    
