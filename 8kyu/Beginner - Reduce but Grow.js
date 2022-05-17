f096e1a4584f4f43a52679bf64a98cfaf9c78494
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-05-17 14:47:49 -0400

    added solutions

--- 8kyu/Beginner - Reduce but Grow.js
+++ 8kyu/Beginner - Reduce but Grow.js
@@ -10,3 +10,22 @@
 
 // Solution:
 
+
+
+function grow(x){
+    return x.reduce((a,b) => a*b,1)
+  }
+  
+  
+  const grow=x=> x.reduce((a,b) => a*b);
+  
+  
+  const grow = x => {
+    let res = 1;
+    for (let i = 0; i < x.length; i++) {
+      res *= x[i];
+    }
+    return res;
+  };
+
+  
