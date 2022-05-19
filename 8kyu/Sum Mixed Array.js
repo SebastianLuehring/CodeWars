0ac09406c92c6b2513bbe1955d64cb7d9a0ff3bf
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-05-19 14:52:18 -0400

    added solutions

--- 8kyu/Sum Mixed Array.js
+++ 8kyu/Sum Mixed Array.js
@@ -12,3 +12,31 @@
 // Solution:
 
 
+function sumMix(x){
+    let newX = x.map(Number)
+    return (newX.reduce((a,b) => a + b,0))
+  }
+  
+  
+  function sumMix(x){
+    return x.map(a => +a).reduce((a, b) => a + b);
+  }
+  
+  
+  
+  function sumMix(x){
+    let result = 0;
+    for (let n of x) {
+      result += parseInt(n);
+    }
+    return result;
+  }
+  
+  
+  
+  function sumMix(x){
+   return x.reduce((s,x)=>s+ +x,0);
+  }
+
+  
+  
