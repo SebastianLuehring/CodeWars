97a7ab33a2a046df47bb11ce8224c016368526b7
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-05-20 14:53:51 -0400

    question prompt

--- 8kyu/Array plus array.js
+++ 8kyu/Array plus array.js
@@ -11,3 +11,13 @@
 // Solution:
 
 
+function arrayPlusArray(arr1, arr2) {
+    return arr1.reduce((a,b) => a + b,0) + arr2.reduce((a,b) => a + b,0); //something went wrong
+  }
+  
+  // accepts indefinite number of arrays
+  function arrayPlusArray(...arrays) {
+    return [].concat(...arrays).reduce((a,b) => a+b,0)
+  }
+
+  
