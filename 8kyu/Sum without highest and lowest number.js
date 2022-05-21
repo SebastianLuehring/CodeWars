cf04b5069603b9c7b65ac94078b85ecabd456068
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-05-21 14:55:45 -0400

    added solutions

--- 8kyu/Sum without highest and lowest number.js
+++ 8kyu/Sum without highest and lowest number.js
@@ -18,3 +18,40 @@
 // Solution:
 
 
+function sumArray(array) {
+    if (array == null) {
+      return 0;
+    } else if (array.length < 2) {
+      return 0;
+    } else {
+      array = array.sort(function(a,b) {return a - b;});
+      var total = 0;
+      for (var i = 1; i < array.length - 1; i++) {
+        total += array[i];
+      }
+      return total;
+    }
+  }
+  
+  
+  
+  function sumArray(array)  {
+      if(array && array.length > 1) {
+          const sortedArray = array.sort((a,b) => a - b).slice(1, -1)
+          return sortedArray.reduce((acc, cur) => acc + cur, 0)
+      }
+  
+      return 0
+  }
+  
+  
+  
+  const sumNums = (acc, cur) => acc + cur
+  
+  function sumArray(array) {
+      return Array.isArray(array) && array.length > 1
+              ? array.reduce(sumNums, 0) - Math.min(...array) - Math.max(...array)
+              : 0
+  }
+
+  
