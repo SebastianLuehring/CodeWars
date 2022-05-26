12cb43322035487f7b0e96a46b5e3b0d2b4dfa7d
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-05-26 11:16:34 -0400

    added solutions

--- 8kyu/The Feast of Many Beasts.js
+++ 8kyu/The Feast of Many Beasts.js
@@ -10,3 +10,33 @@
 
 // Solution:
 
+
+function feast(beast, dish) {
+    if(beast.charAt(0) +  beast.charAt(beast.length -1) === dish.charAt(0) +  dish.charAt(dish.length - 1)){
+       return true
+    }else{
+      return false
+    }
+    
+  }
+  
+  
+  function feast(beast, dish) {
+    return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
+  }
+  
+  
+  
+  function feast(beast, dish) {
+    return beast[0]===dish[0] && beast.slice(-1)===dish.slice(-1);
+  }
+  
+  
+  
+  function feast(beast, dish) {
+    return dish.startsWith(beast[0]) && dish.endsWith(beast[beast.length-1])
+  }
+
+  
+
+  
