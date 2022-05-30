8c041dd931721800615f4411afff7deaa7770733
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-05-30 11:28:25 -0400

    added solutions

--- 8kyu/Well of Ideas - Easy Version.js
+++ 8kyu/Well of Ideas - Easy Version.js
@@ -12,3 +12,37 @@
 // Solution:
 
 
+function well(x) {
+    switch (x.filter(i => i === 'good').length) {
+      case 0:
+        return 'Fail!'
+      case 1:
+      case 2:
+        return 'Publish!'
+      default:
+        return 'I smell a series!'
+    }
+  }
+  
+  
+  
+  const well = x => {
+    const good_count = x.filter(x => x == 'good').length;
+    return good_count < 1 ? 'Fail!' : 
+           good_count < 3 ? 'Publish!' : 'I smell a series!';
+  }
+  
+  
+  
+  function well(x) {
+    var good = 0
+    
+    for (var i = 0; i < x.length; ++i)
+      if (x[i] == 'good' && ++good > 2)
+        return 'I smell a series!'
+    
+    return good ? 'Publish!' : 'Fail!'
+  }
+
+  
+  
