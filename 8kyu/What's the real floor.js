b7c29a17d2936fcb13ccab3749be5245de9304ad
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-06-02 11:33:17 -0400

    added solutions

--- 8kyu/What's the real floor.js
+++ 8kyu/What's the real floor.js
@@ -20,3 +20,44 @@
 // Solution:
 
 
+function getRealFloor(n) {
+    if(n === 1){
+      return 0
+    }else if(n === 0 ){
+      return 0
+    }else if(n < 0){
+      return n
+    }else if (n > 13){
+      return n - 2
+    }else if (n === 13){
+      return 14
+    }else if(n < 13){
+      return n - 1
+    }
+  }
+  
+  
+  
+  
+  function getRealFloor(n) {
+    return n > 13 ? n - 2 : n > 0 ? n - 1 : n;
+  }
+  
+  
+  const getRealFloor = n => {
+    if(n >= 13) return n - 2
+    if(n > 0) return n - 1
+    return n
+  }
+  
+  
+  
+  function getRealFloor(n) {
+    // Less than 1, return n
+    if (n <= 0) return n;
+    
+    return n - (n >= 13 ? 2 : 1);
+  }
+
+  
+  
