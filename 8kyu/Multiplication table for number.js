18b8ceefb2754ddbf1061b9da4d7bd6035de5022
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-06-04 11:38:25 -0400

    added solutions

--- 8kyu/Multiplication table for number.js
+++ 8kyu/Multiplication table for number.js
@@ -24,3 +24,23 @@
 // Solution:
 
 
+function multiTable(number) {
+    return [1,2,3,4,5,6,7,8,9,10].map(item => `${item} * ${number} = ${item*number}`).join('\n');
+    }
+    
+    
+    
+    const multiTable = (number) => {
+      let table = '';
+      
+      for(let i = 1; i <= 10; i++) {
+        table += `${i} * ${number} = ${i * number}${i < 10 ? '\n' : ''}`; 
+      }
+    
+      return table;
+    }
+    
+    
+    
+    
+    
