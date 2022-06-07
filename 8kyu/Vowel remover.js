9835357c5e395aca307c33a0808710a18ac26f00
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-06-07 11:45:09 -0400

    added solutions

--- 8kyu/Vowel remover.js
+++ 8kyu/Vowel remover.js
@@ -17,3 +17,13 @@
 // Solution:
 
 
+function shortcut(string){
+    return string.replace(/[aeiou]+/g, "");
+  }
+  
+  
+  function shortcut(str) {
+    return str.split('').filter(function(e) {
+      return ['a', 'e', 'i', 'o', 'u'].indexOf(e) == -1 
+    }).join('')
+  }
