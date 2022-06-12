b6442c9691fe57cbf73444f1837d2ab6c84330ee
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-06-12 11:57:35 -0400

    added solutions

--- 8kyu/Super Duper Easy.js
+++ 8kyu/Super Duper Easy.js
@@ -11,3 +11,21 @@
 // Solution:
 
 
+function problem(x){
+    if(typeof x === 'string'){
+      return "Error"
+    }else {
+      return (x * 50 + 6) 
+    }
+  }
+  
+  
+  const problem = x => typeof x === 'string' ? 'Error' : x * 50 + 6;
+  
+  
+  function problem(x){
+    return typeof x === "number" ? x * 50 + 6 : "Error";
+  }
+
+  
+  
