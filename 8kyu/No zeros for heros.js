d97d4ffe7b8acb8c3f1ad37df597c30f925a43ee
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-06-14 11:59:52 -0400

    added solutions

--- 8kyu/No zeros for heros.js
+++ 8kyu/No zeros for heros.js
@@ -15,3 +15,27 @@
 // Solution:
 
 
+function noBoringZeros(n) {
+    const numStr = String(n).split('');
+    // your code
+    
+    if (n === 0) {
+      return 0;
+    }
+    
+    for (let i = numStr.length-1; i >= 0; i -= 1) {
+      
+      if (numStr[i] !== '0') {
+        return +(numStr.slice(0, i+1).join(''));
+      }
+  
+    }
+  }
+  
+  
+  function noBoringZeros(n) {
+    while(n%10==0 && n!=0){n/=10;}
+    return n;
+  }
+
+  
