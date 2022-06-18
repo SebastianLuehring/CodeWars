b43a7368dc5f382c6f36af2107de7fb6f6b454e1
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-06-18 12:10:25 -0400

    added solutions

--- 8kyu/For UFC Fans (Total Beginners) Conor McGregor vs George Saint Pierre.js
+++ 8kyu/For UFC Fans (Total Beginners) Conor McGregor vs George Saint Pierre.js
@@ -18,3 +18,45 @@
 // Solution:
 
 
+var quote = function(fighter) {
+    if(fighter ==="George Saint Pierre" || fighter.toLowerCase() === "george saint pierre"){
+      return `I am not impressed by your performance.`
+    }else if(fighter === "Conor McGregor" ||fighter.toLowerCase() === "conor mcgregor"  ){
+      return `I'd like to take this chance to apologize.. To absolutely NOBODY!`
+    }
+  };
+  
+  
+  
+  var quote = function(fighter) {
+    switch(fighter.toLowerCase()) {
+      case 'conor mcgregor': return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+      case 'george saint pierre': return "I am not impressed by your performance.";
+      default : 'Who are you?' 
+    }
+  };
+  
+  
+  var quote = function(fighter) {
+    return fighter.toLowerCase() === "george saint pierre" ? "I am not impressed by your performance." : "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+  };
+  
+  
+  
+  var quote = function(fighter) {
+    if (fighter === 'george saint pierre') {
+      return  "I am not impressed by your performance.";
+    } else if (fighter ===  'Conor McGregor') {
+    return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+    } else if (fighter === 'George Saint Pierre') {
+    return  "I am not impressed by your performance.";
+    } else if (fighter ===  'conor mcgregor'){
+   return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+    } else if (fighter === 'GEORGE SAINT PIERRE') {
+    return  "I am not impressed by your performance.";
+    } else if (fighter ===  'CONOR MCGREGOR'){
+   return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+    } 
+  };
+
+  
