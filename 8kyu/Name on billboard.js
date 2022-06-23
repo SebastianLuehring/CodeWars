b4510e884f5728bb7e4121419a4bfc1e5d7b214e
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-06-23 12:28:39 -0400

    added solutions

--- 8kyu/Name on billboard.js
+++ 8kyu/Name on billboard.js
@@ -11,3 +11,21 @@
 // Solution:
 
 
+function billboard(name, price = 30){
+    return (name.length) / (1 / price);
+  }
+  
+  
+  function billboard(name, price = 30){
+  
+  var totalCost = 0;
+  for(i=0; i<name.length; i++){
+      totalCost += price;
+  } 
+  
+  return totalCost;
+  
+  }
+  
+  
+  
