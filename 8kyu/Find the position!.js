4e756bbf8de282206853cb7c1611c5f3224171f3
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-07-13 14:07:51 -0400

    added solution

--- 8kyu/Find the position!.js
+++ 8kyu/Find the position!.js
@@ -14,3 +14,19 @@
 
 
 
+function position(letter){
+    return "Position of alphabet: " + [...letter].map(a => parseInt(a, 36) - 9).filter(a => a >= 0);
+    }
+    
+    
+    
+    function position(letter){
+      const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+      return 'Position of alphabet: ' + (alphabet.indexOf(letter) + 1);
+    }
+    
+    
+    function position(letter) {
+      return `Position of alphabet: ${letter.charCodeAt(0) - 96}`
+    }
+    
