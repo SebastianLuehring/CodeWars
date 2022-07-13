552d6222dbe9197c5c51320acca742d8cb24192e
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-07-13 14:07:17 -0400

    question prompt

---
+++ 8kyu/Find the position!.js
@@ -0,0 +1,16 @@
+// Find the position!
+
+
+
+
+
+// When provided with a letter, return its position in the alphabet.
+// Input :: "a"
+// Ouput :: "Position of alphabet: 1"
+
+
+
+// Solution:
+
+
+
