eeb75f7b7204239683f94ceb991c6a088211596b
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-07-17 13:53:53 -0400

    added solutions

--- 8kyu/Is it a palindrome.js
+++ 8kyu/Is it a palindrome.js
@@ -12,3 +12,21 @@
 // Solution:
 
 
+function isPalindrome(x) {
+    var re = /[\W_]/g;
+  var lowRegStr = x.toLowerCase().replace(re, '');
+  var reverseStr = lowRegStr.split('').reverse().join(''); 
+  return reverseStr === lowRegStr;
+}
+
+
+const isPalindrome = (x) => {
+  return x.split("").reverse().join("").toLowerCase() === x.toLowerCase() ? true : false
+}
+
+
+const isPalindrome = x => x.toLowerCase().split('').reverse().join('') == x.toLowerCase();
+
+
+
+
