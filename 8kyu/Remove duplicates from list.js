1bf5a7290396165be76cb46a23b582061608344f
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-07-19 13:40:31 -0400

    added solutions

--- 8kyu/Remove duplicates from list.js
+++ 8kyu/Remove duplicates from list.js
@@ -10,3 +10,40 @@
 // Solution:
 
 
+function distinct(a) {
+    var uniqueArray = [];
+          
+          // Loop through array values
+          for(i=0; i < a.length; i++){
+              if(uniqueArray.indexOf(a[i]) === -1) {
+                  uniqueArray.push(a[i]);
+              }
+          }
+          return uniqueArray;
+      }
+  
+  
+  function distinct(a) {
+    return [...new Set(a)];
+  }
+  
+  
+  function distinct(a) {
+    return Array.from(new Set(a));
+  }
+  
+  
+  function distinct(arr) {
+    let res = []; 
+    
+    for(let i = 0; i < arr.length; i++){
+      if(!res.includes(arr[i])){
+        res.push(arr[i]);
+      }
+  }
+  return res;
+  }
+  
+
+  
+  
