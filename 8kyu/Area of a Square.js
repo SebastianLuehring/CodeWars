5800819c391b604fb80e79c331b38d9d8179241a
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-07-23 11:30:16 -0400

    added solutions

--- 8kyu/Area of a Square.js
+++ 8kyu/Area of a Square.js
@@ -11,3 +11,26 @@
 // Solution:
 
 
+function squareArea(A){
+    return +Math.pow(2 * A / Math.PI, 2).toFixed(2)
+  }
+  
+  
+  function squareArea(A){
+    var circum = 4 * A;
+    var radius = circum / (2 * Math.PI);
+    var area = Math.pow(radius, 2);
+    return Math.round(area*100)/100
+  }
+  
+  
+  squareArea = A => +Math.pow((2 * A / 3.1416), 2).toFixed(2);
+  
+  
+  function squareArea(A){
+    return Math.round(Math.pow(A*2/Math.PI,2) * 100) /100
+  }
+  
+
+  
+  
