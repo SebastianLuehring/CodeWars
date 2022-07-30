d6181f6ea4ba7c4b78a03dd7a2acc4705c380e70
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-07-30 11:39:52 -0400

    added solutions

--- 8kyu/Exclamation marks series #2 Remove all exclamation marks from the end of sentence.js
+++ 8kyu/Exclamation marks series #2 Remove all exclamation marks from the end of sentence.js
@@ -18,3 +18,31 @@
 // Solution:
 
 
+function remove (string) {  
+    while (string && string.slice(-1) == '!') {
+    string = string.slice(0,-1);
+    }
+    return string;
+  }
+  
+  
+  
+  const remove = s => s.replace(/!+$/, '');
+  
+  
+  
+  function remove(s){
+    return s.replace(/!+$/, '');
+  }
+  
+  
+  function remove(s){
+    //coding and coding....
+    while (s[s.length - 1] === '!') {
+      s = s.slice(0, -1);
+    }
+    return s;
+  }
+
+  
+  
