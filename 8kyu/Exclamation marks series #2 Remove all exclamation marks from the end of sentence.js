8656ac58729278f568a6cdd882cf82414e0dd8b7
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-07-30 11:39:15 -0400

    question prompt

---
+++ 8kyu/Exclamation marks series #2 Remove all exclamation marks from the end of sentence.js
@@ -0,0 +1,20 @@
+// Exclamation marks series #2: Remove all exclamation marks from the end of sentence
+
+
+
+
+
+// Remove all exclamation marks from the end of sentence.
+// Examples
+// remove("Hi!") === "Hi"
+// remove("Hi!!!") === "Hi"
+// remove("!Hi") === "!Hi"
+// remove("!Hi!") === "!Hi"
+// remove("Hi! Hi!") === "Hi! Hi"
+// remove("Hi") === "Hi"
+
+
+
+// Solution:
+
+
