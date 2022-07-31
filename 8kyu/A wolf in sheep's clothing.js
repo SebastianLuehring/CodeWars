af1cbb910eaf7e104a9c8788d68d5f9d9647bc3a
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-07-31 11:41:52 -0400

    added solutions

--- 8kyu/A wolf in sheep's clothing.js
+++ 8kyu/A wolf in sheep's clothing.js
@@ -19,3 +19,39 @@
 // Solution:
 
 
+function warnTheSheep(queue) {
+    if(queue.length - 1 === queue.indexOf("wolf")){
+      return "Pls go away and stop eating my sheep"
+    }else{
+      return `Oi! Sheep number ${queue.length - 1 - queue.indexOf("wolf")  }! You are about to be eaten by a wolf!`
+    }
+  }
+  
+  
+  function warnTheSheep(queue) {
+    const position = queue.reverse().indexOf('wolf');
+    return position === 0 ? 'Pls go away and stop eating my sheep' : `Oi! Sheep number ${ position }! You are about to be eaten by a wolf!`;
+  }
+  
+  
+  
+  
+  function warnTheSheep(q) {
+    return q[q.length-1] === 'wolf' ? "Pls go away and stop eating my sheep" : `Oi! Sheep number ${q.length - (q.indexOf('wolf')+1)}! You are about to be eaten by a wolf!`
+  }
+  
+  
+  
+  function warnTheSheep(queue) {
+      let sheepIndex = queue.length - queue.indexOf('wolf') - 1;
+      if (sheepIndex === 0)
+          return 'Pls go away and stop eating my sheep';
+      return `Oi! Sheep number ${sheepIndex}! You are about to be eaten by a wolf!`;
+  }
+  
+  
+  
+
+  
+
+  
