5f74b7c29658fd43f46a8aff55fdbfa7d43a4eff
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-08-03 11:45:43 -0400

    question prompt

---
+++ 8kyu/Ghost code!.js
@@ -0,0 +1,33 @@
+// Ghost code?!
+
+
+
+// Trollencio changed my code! can you fix it? My name is not "dumb!" o( TT_TT )o
+// Instructions: Fix the code
+// Input
+// Javatlacati
+// Output
+// Hello my name is Javatlacati
+// Input
+// ""
+// Output
+// Hello world!
+
+
+
+// Trollencio changed my code! can you fix it? My name is not "dumb!" o( TT_TT )o
+// Instructions: Fix the code
+// Input
+// Javatlacati
+// Output
+// Hello my name is Javatlacati
+// Input
+// ""
+// Output
+// Hello world!
+
+
+
+// Solutoion:
+
+
