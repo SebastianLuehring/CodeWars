b389a94471cda47d62d34e847179a477255a16c6
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-08-03 11:46:27 -0400

    added solution

--- 8kyu/Ghost code!.js
+++ 8kyu/Ghost code!.js
@@ -28,6 +28,23 @@
 
 
 
-// Solutoion:
+// Solution:
 
+public class GhostCode{
+    public static String helloName(final String name){
+    
+      if(name == null || name.equals(""))
+        return "Hello world!";
+      else{
+        return "Hello my name is " + name;
+      }
+    }
+  }
+  
+  
+  
+  
+  
 
+  
+  
