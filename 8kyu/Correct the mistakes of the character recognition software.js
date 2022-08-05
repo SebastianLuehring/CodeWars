c76d6b4542a64358405b4f538f65bc43eb9ddcae
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-08-05 14:51:34 -0400

    added solutions

--- 8kyu/Correct the mistakes of the character recognition software.js
+++ 8kyu/Correct the mistakes of the character recognition software.js
@@ -18,3 +18,49 @@
 // Solution:
 
 
+function correct(string){
+    const chars = {'5':'S','0':'O','1':'I'};
+return string.replace(/[501]/g, m => chars[m]);
+}
+
+
+correct = s => s.replace(/0/g,'O').replace(/1/g,'I').replace(/5/g,'S')
+
+
+
+
+const corrections = {
+  '5': 'S',
+  '0': 'O',
+  '1': 'I',
+};
+
+const correct = string => (
+  string.replace(/[501]/g, character => corrections[character])
+);
+
+
+
+function correct(string) {
+    return string.replace(/0/g, "O")
+                 .replace(/5/g, "S")
+                 .replace(/1/g, "I");
+}
+
+
+
+const correct = string => string.replace(/0/g, "O").replace(/1/g, "I").replace(/5/g, "S");
+
+
+function correct(s){
+  s = s.split('');
+  for (var i = 0; i < s.length; i++) {
+    if (s[i] === '5') s[i] = 'S';
+    else if (s[i] === '0') s[i] = 'O';
+    else if (s[i] === '1') s[i] = 'I';
+    }
+  return s.join('');
+}
+
+
+
