f30547919a32c1d8e0a4bd85f9eeff5ceeeebd67
Sebastian Luehring <sebastianluehring00@gmail.com>
2022-10-01 13:50:37 -0400

    added solutions

--- 8kyu/Barking mad.js
+++ 8kyu/Barking mad.js
@@ -16,3 +16,20 @@
 // Solution:
 
 
+function Dog (breed) {
+    this.breed = breed;
+  }
+  
+  var snoopy = new Dog("Beagle");
+  
+  snoopy.bark = function() {
+    return "Woof";
+  };
+  
+  var scoobydoo = new Dog("Great Dane");
+  
+  scoobydoo.bark = function() {
+    return "Woof"
+  }
+
+  
